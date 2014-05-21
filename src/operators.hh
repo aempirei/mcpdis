0163dfbc1ef999ec96c3b2d632dd01f6428e5a7c
Christopher Abad <aempirei@256.bz>
2014-05-21 05:03:40 -0700

    .

---
+++ src/operators.hh
@@ -0,0 +1,12 @@
+#pragma once
+
+#define OP_AND		L'\u2227'
+#define OP_OR		L'\u2228'
+#define OP_XOR		L'\u2297'
+#define OP_PLUS		L'+'
+#define OP_MINUS	L'-'
+#define OP_SWAP		L'\u225c'
+#define OP_NOT		L'\u00ac'
+#define OP_ROTL		L'\u21a9'
+#define OP_ROTR		L'\u21aa'
+#define OP_COMPOSE	L'\u2218'
