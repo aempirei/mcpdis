ca9cdff371095d456a50a2149aa51aa52532470f
Christopher Abad <aempirei@256.bz>
2014-05-25 23:03:21 -0700

    .

--- Makefile
+++ Makefile
@@ -18,11 +18,11 @@ test: all
 	cat test.dis
 	md5sum test.dis
 
-src/mcpdis.o: src/mcpdis.hh src/operators.hh src/ansicolor.hh src/mcpdis.cc src/term.cc src/pic12f.cc src/parser.cc
+src/mcpdis.o: src/mcpdis.hh src/operators.hh src/ansicolor.hh src/mcpdis.cc src/term.cc src/pic12f.cc src/parser.cc src/function.cc
 
 src/dis.o: src/dis.cc src/mcpdis.hh
 
-lib/libmcpdis.a: src/mcpdis.o src/term.o src/pic12f.o src/parser.o
+lib/libmcpdis.a: src/mcpdis.o src/term.o src/pic12f.o src/parser.o src/function.o
 	if [ ! -d lib ]; then mkdir -vp lib; fi
 	ar crfv $@ $^ 
 
--- src/operators.hh
+++ src/operators.hh
@@ -4,6 +4,7 @@
 #define OP_OR		L'\u222a'
 #define OP_XOR		L'\u2295'
 #define OP_PLUS		L'+'
+
 #define OP_MINUS	L'-'
 #define OP_COMPOSE	L'\u2218'
 #define OP_LIST		L','
@@ -12,66 +13,3 @@
 #define OP_NOT		L'\u00ac'
 #define OP_ROTL		L'\u21a9'
 #define OP_ROTR		L'\u21aa'
-
-#define OP_IS_NILPOTENT(x)	\
-	(			\
-	 ((x) == OP_XOR)	\
-	)
-
-#define OP_IS_IDEMPOTENT(x)	\
-	(			\
-	 ((x) == OP_AND) ||	\
-	 ((x) == OP_OR )	\
-	)
-
-#define OP_IS_ASSOCIATIVE(x)	\
-	(			\
-	 ((x) == OP_AND ) ||	\
-	 ((x) == OP_OR  ) ||	\
-	 ((x) == OP_XOR ) ||	\
-	 ((x) == OP_PLUS)	\
-	)
-
-#define OP_IS_COMMUTATIVE(x)	\
-	(			\
-	 ((x) == OP_AND ) ||	\
-	 ((x) == OP_OR  ) ||	\
-	 ((x) == OP_XOR ) ||	\
-	 ((x) == OP_PLUS) 	\
-	)		
-
-#define OP_IS_UNARY(x)		\
-	(			\
-	 ((x) == OP_NOT ) ||	\
-	 ((x) == OP_SWAP) ||	\
-	 ((x) == OP_ROTL) ||	\
-	 ((x) == OP_ROTR) 	\
-	)
-
-#define OP_IS_UNARY_NOP(x)	\
-	(			\
-	 ((x) == OP_AND    ) ||	\
-	 ((x) == OP_OR     ) ||	\
-	 ((x) == OP_XOR    ) ||	\
-	 ((x) == OP_PLUS   ) ||	\
-	 ((x) == OP_MINUS  ) ||	\
-	 ((x) == OP_COMPOSE) 	\
-	)
-
-#define OP_IDENTITY_IS_0(x)	\
-	(			\
-	 ((x) == OP_OR  ) ||	\
-	 ((x) == OP_XOR ) ||	\
-	 ((x) == OP_PLUS)	\
-	)
-
-#define OP_IDENTITY_IS_1(x)	\
-	(			\
-	 ((x) == OP_AND)	\
-	)
-
-#define OP_IS_UNARY_INVOLUTION(x)	\
-	(				\
-	 ((x) == OP_NOT ) ||		\
-	 ((x) == OP_SWAP)		\
-	)
