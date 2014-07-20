78f8e955b5122212d4a7dbac0ebd1b41f8cf07c3
Christopher Abad <aempirei@256.bz>
2014-07-19 17:01:17 -0700

    .

--- src/yyy/expr.template.hh
+++ src/yyy/expr.template.hh
@@ -2,22 +2,56 @@
 
 namespace yyy {
 
-	template <typename T, typename U> using _expr = std::pair<T,std::list<U>>;
-
-	template <typename T, typename U> struct expr : _expr<T,U> {
+	template <typename T, typename U> struct expr {
 
 		using head_type = T;
-		using argument_type = U;
-		using tail_type = std::list<U>;
+		using argument_type = typename U::template append<expr>;
+		using tail_type = std::list<argument_type>;
 
-		using _expr<T,U>::_expr;
+		head_type op;
+		tail_type args;
 
-		expr(const head_type& head) : expr(head, {}) {
+		expr() : op(), args() {
+		}
+
+		expr(const head_type& my_op) : op(my_op), args({}) {
+		}
+
+		expr(const head_type& my_op, const tail_type& my_args) : op(my_op), args(my_args) {
+		}
+
+		expr(const expr& r) : op(r.op), args(r.args) {
+		}
+
+		expr& operator=(const expr& r) {
+			if(this != &r) {
+				op = r.op;
+				args = r.args;
+			}
+			return *this;
 		}
 
 		expr& operator<<(const argument_type& arg) {
-			this->second.push_back(arg);
+			args.push_back(arg);
 			return *this;
+		}
+
+		expr& operator<<(const expr& x) {
+			args.push_back(argument_type(x));
+			return *this;
+		}
+
+		template <typename X> expr& operator<<(const X& x) {
+			args.push_back(argument_type(x));
+			return *this;
+		}
+
+		bool operator==(const expr& r) const {
+			return op == r.op and args == r.args;
+		}
+
+		bool operator!=(const expr& r) const {
+			return not operator==(r);
 		}
 
 		std::wstring str() const {
